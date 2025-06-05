952c2d293965e3ce3c664194b00d228a207668a0
gitdallas <5322142+gitdallas@users.noreply.github.com>
2025-06-05 11:04:52 -0500

    fix a prop type that codemods missed
    Signed-off-by: gitdallas <5322142+gitdallas@users.noreply.github.com>

--- libs/ui-lib/lib/common/components/ui/Toolbar/ToolbarText.tsx
+++ libs/ui-lib/lib/common/components/ui/Toolbar/ToolbarText.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ToolbarItem, Content } from '@patternfly/react-core';
 
-const ToolbarText: React.FC<React.ComponentProps<typeof Text>> = (props) => (
+const ToolbarText: React.FC<React.ComponentProps<typeof Content>> = (props) => (
   <ToolbarItem>
     <Content component="p" {...props} />
   </ToolbarItem>
